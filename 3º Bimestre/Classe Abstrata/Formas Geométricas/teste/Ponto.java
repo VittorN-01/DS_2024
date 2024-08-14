cd0e6c796c3145e07b6523f7eb3eb5cf6445a2f5
Vittor Nascimento Sirqueira <144181234+VittorN-01@users.noreply.github.com>
2024-08-14 08:07:34 -0300

    Add files via upload

---
+++ 3º Bimestre/Classe Abstrata/Formas Geométricas/teste/Ponto.java
@@ -0,0 +1,13 @@
+public class Ponto extends FormaGeometrica {
+	private double coordx;
+	private double coordy;
+	
+	public Ponto(double x, double y) {
+		coordx = x;
+		coordy = y;
+	}
+	
+	public String forma() {
+		return "Ponto: (" + coordx + ", " + coordy + ")";
+	}
+}
