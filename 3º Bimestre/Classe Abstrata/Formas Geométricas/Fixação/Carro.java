5daed879c20ff70a9e4f67053483a4d0d9e08cdc
Vittor Nascimento Sirqueira <144181234+VittorN-01@users.noreply.github.com>
2024-09-12 20:09:00 -0300

    Delete 3º Bimestre/Classe Abstrata/Formas Geométricas/Fixação directory

--- 3º Bimestre/Classe Abstrata/Formas Geométricas/Fixação/Carro.java
+++
@@ -1,7 +0,0 @@
-public class Carro extends Veículo{
-	@Override
-	public double calcularIpva(double valorTabela) {
-		super.calcularIpva(getValorTabela());
-		return valorTabela*0.04/100;
-	}
-}
--- 3º Bimestre/Classe Abstrata/Formas Geométricas/Fixação/Moto.java
+++
@@ -1,7 +0,0 @@
-public class Moto extends Veículo{
-	@Override
-	public double calcularIpva(double valorTabela) {
-		super.calcularIpva(getValorTabela());
-		return valorTabela*0.02/100;
-	}
-}
