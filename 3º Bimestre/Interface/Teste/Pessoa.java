30585c954bf8885a188da74d629f19ed08af1d34
Vittor Nascimento Sirqueira <144181234+VittorN-01@users.noreply.github.com>
2024-10-02 20:43:48 -0300

    Delete 3º Bimestre/Interface/Teste directory

--- 3º Bimestre/Interface/Teste/Pessoa.java
+++
@@ -1,9 +0,0 @@
-public interface Pessoa {
-	String getNome();
-	void setNome(String nome);
-	String getEndereco();
-	String setEndereco (String endereco);
-	String getTelefone();
-	String setTelefone(String Telefone);
-	void apresentar();
-}
